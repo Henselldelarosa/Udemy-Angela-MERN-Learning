40b27c2f32ed512837949f9218d933d0e4e4143d
hensell de la rosa <hensell1234@gmail.com>
2024-01-02 21:53:41 -0500

    session 15 lesson 1 done

---
+++ 15/loveCalculator.js
@@ -0,0 +1,15 @@
+
+// const loveCalculator = () => {
+//   prompt("What is your Name")
+//   prompt("What is their Name")
+//   //  return alert((Math.random() * 100) + 1)
+// }
+
+// loveCalculator()
+
+prompt("What is your Name")
+prompt("What is their Name")
+let loveScore = Math.random() * 100
+loveScore = Math.floor(loveScore) + 1
+console.log(loveScore)
+alert('Your Love Score Is ' + loveScore + '%')
