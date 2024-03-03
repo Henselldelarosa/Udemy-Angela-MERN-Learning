243419b110d98217fc4445b83463974aaca97e02
hensell de la rosa <hensell1234@gmail.com>
2024-03-03 18:24:34 -0500

    pattern game done

--- 20/simon-Game-Challenge-Starting-Files/index.js
+++ 20/simon-Game-Challenge-Starting-Files/index.js
@@ -1,25 +1,79 @@
-const gameStart = false
+let gameStart = false
 let level = 0
-let nextPattern = []
+let nextSequence = []
+let userPressedPattern = []
+let colorChoice = ['green', 'red', 'yellow', 'blue']
 
-// $('div.btn').on('click', function(){
-//   let audio = new Audio('sounds/wrong.mp3')
-//   audio.play()
-// })
 
 $(document).keypress(function() {
-
   if(!gameStart){
-    $('#level-title').text(`Level ${level + 1}`)
+    $('#level-title').text('Level ' + level)
+    pattern()
     gameStart = true
   }
 })
 
-// if(!gameStart){
-//   $('div.btn').on('click', function(){
-//     let audio = new Audio('sounds/wrong.mp3')
-//     audio.play()
-//   })
+$('.btn').on('click', function() {
+  let userChoice = $(this).attr('id')
+  userPressedPattern.push(userChoice)
+  playSound(userChoice)
+  animate(userChoice)
+  checkAnswer(userPressedPattern.length - 1)
+})
 
-//   $('#level-title').text('Game Over Press Any Key to Restart')
-// }
+const animate = (color) => {
+  $('#' + color).addClass('pressed')
+
+  setTimeout(() => {
+    $('#' + color).removeClass('pressed')
+  }, 100);
+}
+
+const playSound = (name) => {
+  const audio = new Audio('sounds/' + name + '.mp3')
+  audio.play()
+}
+
+const pattern = () => {
+  userPressedPattern = []
+  level++
+
+  $('#level-title').text('Level ' + level)
+
+  let randomeChoice = Math.floor(Math.random() * 4)
+  let color = colorChoice[randomeChoice]
+
+  nextSequence.push(color)
+
+  $('#' + color).fadeIn(100).fadeOut(100).fadeIn(100)
+  playSound(color)
+}
+
+const checkAnswer = (level) => {
+  if(nextSequence[level] === userPressedPattern[level]){
+    if(userPressedPattern.length === nextSequence.length){
+      setTimeout(() => {
+        pattern()
+      }, 1000);
+    }
+  }
+
+  else{
+    playSound('wrong')
+    $('body').addClass('game-over')
+    $('#level-title').text('Game Over, Press Any Key to Restart')
+
+    setTimeout(function() {
+      $('body').removeClass('game-over')
+    }, 200);
+
+    startOver()
+  }
+}
+
+const reStart = () => {
+  level = 0
+  userPressedPattern = []
+  nextSequence = []
+  gameStart = false
+}
