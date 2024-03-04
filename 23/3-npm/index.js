d68bea7ad681adb4cbe850e75ad04721295d2682
hensell de la rosa <hensell1234@gmail.com>
2024-03-04 17:11:34 -0500

    197 done

---
+++ 23/3-npm/index.js
@@ -0,0 +1,8 @@
+// const generateName = require('sillyname')
+// const sillyname = generateName()
+// console.log(`My name is ${superheroesName}`)
+// const superHeroes = require('superheroes')
+import superheroes from'superheroes'
+
+const name = superheroes.random()
+console.log(`My hero name is ${name}`)
