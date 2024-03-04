224a9eb26f853b55c5dd56a62a7e8dcdd1f2127f
hensell de la rosa <hensell1234@gmail.com>
2024-03-04 16:22:02 -0500

    196 done

--- 23/2-native-Modules/index.js
+++ 23/2-native-Modules/index.js
@@ -1,1 +1,14 @@
-console.log('hello')
+const fs = require("fs")
+
+fs.writeFile('message.txt', 'Hello from NodeJS', (err) => {
+  if(err) throw err;
+
+  console.log('This file has been saved!')
+})
+
+
+fs.readFile('./message.txt', 'utf8', (err, data) => {
+  if(err) throw err
+
+  console.log(data)
+})
