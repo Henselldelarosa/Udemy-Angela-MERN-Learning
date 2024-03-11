031d4489005c2f458a577d19156ec75172aec5a5
hensell de la rosa <hensell1234@gmail.com>
2024-03-11 03:26:11 -0400

    210 donw

--- 25/4.2 Passing Data/index.js
+++ 25/4.2 Passing Data/index.js
@@ -1,14 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
+import {dirname} from 'path'
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
 
-app.post("/submit", (req, res) => {});
+
+app.get("/", (req, res) => {
+  const data = {
+    htmlContent: "<h1>Enter Your Name below</h1>",
+  }
+  console.log(data)
+  res.render('index.ejs', data)
+});
+
+app.post("/submit", (req, res) => {
+  const nameLength = req.body['fName'].length + req.body['lName'].length
+  res.render('index.ejs',
+  {name: nameLength})
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
